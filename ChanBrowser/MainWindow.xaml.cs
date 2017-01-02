d9137792cf84abe55deebcdc69c0655ed51ca288
Dillz42 <dillz42@gmail.com>
2017-01-02 16:55:40 -0600

    Changed to Grid and adjust title of window

--- ChanBrowser/MainWindow.xaml.cs
+++ ChanBrowser/MainWindow.xaml.cs
@@ -28,7 +28,7 @@ namespace ChanBrowser
         {
             InitializeComponent();
             WindowState = WindowState.Maximized;
-            KeyDown += (object s, KeyEventArgs e) => 
+            KeyDown += (object s, KeyEventArgs e) =>
             {
                 if (e.Key == Key.OemTilde)
                     MessageBox.Show("Dev console not avaliable!");
@@ -41,10 +41,10 @@ namespace ChanBrowser
             tokenSource.Cancel();
             base.OnClosing(e);
         }
-        
+
         private void loadBoardList()
         {
-            Task<List<Tuple<string, string>>> boardListTask = Global.getBoardList(tokenSource.Token);
+            Task<List<Tuple<string, string, string>>> boardListTask = Global.getBoardList(tokenSource.Token);
             boardListTask.ContinueWith(t =>
             {
                 switch (t.Status)
@@ -57,10 +57,11 @@ namespace ChanBrowser
                             boardButton.Margin = new Thickness(1);
                             boardButton.Content = board.Item1;
                             boardButton.Click += BoardButton_Click;
+                            boardButton.DataContext = board;
                             BoardList.Children.Add(boardButton);
 
                             ToolTip toolTip = new ToolTip();
-                            toolTip.Content = board.Item2;
+                            toolTip.Content = board.Item2 + "\n" + board.Item3;
                             ToolTipService.SetToolTip(boardButton, toolTip);
                         }
                         break;
@@ -96,54 +97,79 @@ namespace ChanBrowser
                      switch (t.Status)
                      {
                          case TaskStatus.RanToCompletion:
-                             Title = "/" + Global.currentBoard + "/";
+                             Title = "/" + ((Tuple<string, string, string>)((Button)sender).DataContext).Item1 + "/ - " +
+                             ((Tuple<string, string, string>)((Button)sender).DataContext).Item2;
 
                              foreach (ChanPost chanThread in Global.chanThreadList)
                              {
-                                 StackPanel threadStackPanel = new StackPanel();
-                                 threadStackPanel.Orientation = Orientation.Vertical;
-                                 threadStackPanel.Margin = new Thickness(3);
-                                 threadStackPanel.MouseUp += ThreadPanel_MouseUp;
+                                 Grid threadGrid = new Grid();
+                                 threadGrid.Margin = new Thickness(3);
+                                 threadGrid.DataContext = chanThread;
+                                 threadGrid.IsHitTestVisible = true;
+                                 threadGrid.MouseLeftButtonUp += ThreadPanel_MouseUp;
+
+                                 threadGrid.RowDefinitions.Add(new RowDefinition());
+                                 threadGrid.RowDefinitions.Add(new RowDefinition());
+                                 threadGrid.RowDefinitions.Add(new RowDefinition());
+                                 threadGrid.RowDefinitions.Add(new RowDefinition());
+
+                                 threadGrid.ColumnDefinitions.Add(new ColumnDefinition());
+                                 threadGrid.ColumnDefinitions.Add(new ColumnDefinition());
+
+                                 {
+                                     Separator separator = new Separator();
+                                     Grid.SetColumn(separator, 0);
+                                     Grid.SetRow(separator, 0);
+                                     Grid.SetColumnSpan(separator, 2);
+                                     threadGrid.Children.Add(separator);
+                                 }
 
                                  //Add meta-data and subject
                                  TextBlock subjectTextBlock = new TextBlock();
+                                 Grid.SetColumn(subjectTextBlock, 0);
+                                 Grid.SetRow(subjectTextBlock, 1);
+                                 Grid.SetColumnSpan(subjectTextBlock, 2);
                                  subjectTextBlock.Foreground = Brushes.White;
                                  subjectTextBlock.TextWrapping = TextWrapping.Wrap;
                                  Global.htmlToTextBlockText(subjectTextBlock,
-                                     "R:" + chanThread.replies + "/I:" + chanThread.images + 
-                                     (chanThread.sub =="" ? "" : " - " + "<strong>" + System.Net.WebUtility.HtmlDecode(chanThread.sub) + "</strong>"));
-                                threadStackPanel.Children.Add(subjectTextBlock);
+                                     "R:" + chanThread.replies + "/I:" + chanThread.images +
+                                     (chanThread.sub == "" ? "" : " - " + "<strong>" + System.Net.WebUtility.HtmlDecode(chanThread.sub) + "</strong>"));
+                                 threadGrid.Children.Add(subjectTextBlock);
 
-                                 StackPanel imageCommentStackPanel = new StackPanel();
-                                 imageCommentStackPanel.Orientation = Orientation.Horizontal;
                                  //Add image
                                  Image postImage = new Image();
-                                 
+                                 Grid.SetColumn(postImage, 0);
+                                 Grid.SetRow(postImage, 2);
                                  postImage.VerticalAlignment = VerticalAlignment.Top;
                                  BitmapImage bitmapImage = new BitmapImage(new Uri(chanThread.imageUrl));
                                  bitmapImage.DownloadCompleted += (ds, de) =>
                                  {
                                      postImage.MaxHeight = bitmapImage.PixelHeight;
                                      postImage.MaxWidth = bitmapImage.PixelWidth;
+                                     threadGrid.ColumnDefinitions[0].MaxWidth = bitmapImage.PixelHeight;
                                  };
                                  postImage.Source = bitmapImage;
-                                 imageCommentStackPanel.Children.Add(postImage);
+                                 threadGrid.Children.Add(postImage);
 
                                  //Add comment
                                  TextBlock commentTextBlock = new TextBlock();
+                                 Grid.SetColumn(commentTextBlock, 1);
+                                 Grid.SetRow(commentTextBlock, 2);
                                  commentTextBlock.Foreground = Brushes.White;
                                  commentTextBlock.Margin = new Thickness(3);
                                  commentTextBlock.TextWrapping = TextWrapping.Wrap;
-                                 Global.htmlToTextBlockText(commentTextBlock,
-                                     System.Net.WebUtility.HtmlDecode(chanThread.com)); ;
-                                 imageCommentStackPanel.Children.Add(commentTextBlock);
+                                 Global.htmlToTextBlockText(commentTextBlock, System.Net.WebUtility.HtmlDecode(chanThread.com));
+                                 threadGrid.Children.Add(commentTextBlock);
 
-                                 threadStackPanel.Children.Add(imageCommentStackPanel);
+                                 {
+                                     Separator separator = new Separator();
+                                     Grid.SetColumn(separator, 0);
+                                     Grid.SetRow(separator, 3);
+                                     Grid.SetColumnSpan(separator, 2);
+                                     threadGrid.Children.Add(separator);
+                                 }
 
-                                 Separator separator = new Separator();
-                                 threadStackPanel.Children.Add(separator);
-
-                                 BoardStackPanel.Children.Add(threadStackPanel);
+                                 BoardStackPanel.Children.Add(threadGrid);
                              }
                              break;
                          case TaskStatus.Canceled:
@@ -161,13 +187,13 @@ namespace ChanBrowser
         {
             if (((FrameworkElement)sender).DataContext != null)
             {
-                ChanPost chanPost = ((ChanPost)((StackPanel)sender).DataContext);
+                ChanPost chanPost = ((ChanPost)((FrameworkElement)sender).DataContext);
 
                 StackPanel stackPanel = new StackPanel();
                 stackPanel.Orientation = Orientation.Vertical;
                 stackPanel.Margin = new Thickness(2);
                 stackPanel.Background = Brushes.DarkGreen;
-                
+
                 Button threadButton = new Button();
                 threadButton.MinWidth = 100;
                 threadButton.Margin = new Thickness(2);
@@ -193,7 +219,7 @@ namespace ChanBrowser
                 ThreadButton_Click(threadButton, null);
             }
         }
-        
+
         private async void ThreadButton_Click(object sender, RoutedEventArgs e)
         {
             ChanPost op = ((ChanPost)((FrameworkElement)sender).DataContext);
@@ -214,10 +240,10 @@ namespace ChanBrowser
                                 postStackPanel.HorizontalAlignment = HorizontalAlignment.Left;
                                 postStackPanel.Margin = new Thickness(2);
                                 postStackPanel.Background = Brushes.MidnightBlue;
-                                    
+
                                 Image image = new Image();
-                                
-                                if(reply.ext != "")
+
+                                if (reply.ext != "")
                                 {
                                     image.Source = new BitmapImage(new Uri(reply.imageUrl));
                                     image.Stretch = Stretch.None;
@@ -239,13 +265,13 @@ namespace ChanBrowser
                                     };
 
                                     toolTipStackPanel.Orientation = Orientation.Vertical;
-                                    
+
                                     toolTipTextBlock.Text = reply.w + "x" + reply.h + " - " + reply.filename + reply.ext;
                                     toolTipStackPanel.Children.Add(toolTipTextBlock);
 
                                     toolTipImage.DataContext = Global.BASE_IMAGE_URL + reply.board + "/" + reply.tim + reply.ext;
                                     toolTipStackPanel.Children.Add(toolTipImage);
-                                    
+
                                     imageToolTip.Content = toolTipStackPanel;
                                     ToolTipService.SetShowDuration(image, int.MaxValue);
                                     //ToolTipService.SetInitialShowDelay(image, 0);
@@ -261,7 +287,7 @@ namespace ChanBrowser
                                 {
                                     TextBlock textBlock = new TextBlock();
                                     Global.htmlToTextBlockText(textBlock,
-                                        (reply.no != 0 ? reply.no + " - " : "") + 
+                                        (reply.no != 0 ? reply.no + " - " : "") +
                                         (reply.name != "" ? reply.name : "Anonymous") + " @ " +
                                         (reply.now != "" ? reply.now : "UNKNOWN TIME"));
                                     textBlock.Foreground = Brushes.White;
@@ -290,18 +316,18 @@ namespace ChanBrowser
                                     Global.htmlToTextBlockText(textBlock, reply.com);
                                     textBlock.Foreground = Brushes.White;
                                     textBlock.TextWrapping = TextWrapping.Wrap;
-                                    textBlock.Loaded += (ls, le) => 
+                                    textBlock.Loaded += (ls, le) =>
                                     {
                                         textBlock.Width = ThreadStackPanel.ActualWidth - image.ActualWidth - SystemParameters.VerticalScrollBarWidth;
                                     };
                                     textStackPanel.Children.Add(textBlock);
                                 }
-                                    
+
                                 postStackPanel.Children.Add(textStackPanel);
 
                                 ThreadStackPanel.Children.Add(postStackPanel);
                             }
-                            
+
                             break;
                         case TaskStatus.Canceled:
                             MessageBox.Show("loadThread was canceled!", "CANCELED");
